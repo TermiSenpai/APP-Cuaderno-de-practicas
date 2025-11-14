8fdff6fde2d7978cd20799414f055527498fcc3f
Termi <escarpaprieto@gmail.com>
2025-11-14 07:21:14 +0100

    vertical background fix

--- CuadernoPracticas/src/App.tsx
+++ CuadernoPracticas/src/App.tsx
@@ -1,11 +1,11 @@
+// App.tsx
 import "./App.css";
 import CuadernoPracticas from "./Activities.tsx";
 import Header from "./Header.tsx";
 
 function App() {
   return (
-    <div className="app-root w-full overflow-x-hidden text-neutral-100">
-    <div className="container-inner px-4 sm:px-5 pt-4 sm:pt-6 pb-6 sm:pb-8 print:px-0 flex-grow"></div>
+    <div className="app-root min-h-screen w-full overflow-x-hidden text-neutral-100 bg-[#0b0f1a] bg-gradient-to-b from-[#0b1220] to-[#0b0f1a]">
       <main className="w-full max-w-[1200px] mx-auto px-4">
         <Header />
         <CuadernoPracticas />
